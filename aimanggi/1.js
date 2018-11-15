fffbbbdf66458c6c7cda8af9f811cddc9820089f
aimanggi <anggimaulinda21@gmail.com>
2018-11-15 07:35:16 +0700

    another progress

--- aimanggi/1.js
+++ aimanggi/1.js
@@ -16,10 +16,6 @@ let product = [
   },
 ];
 
-let nama = document.getElementById("index").value;
-let email = document.getElementById("email").value;
-let alamat = document.getElementById("alamat").value;
-let hp = document.getElementById("hp").value;
 let end = document.getElementById("sudahselesai");
 
 
@@ -38,15 +34,29 @@ else{alert("Masukkan jumlah produk!");
 
 }
 
-
-stock1.innerHTML= product[0].stock - document.getElementById("qty1").value ;
-stock2.innerHTML= product[1].stock - document.getElementById("qty2").value ;
-stock3.innerHTML= product[2].stock - document.getElementById("qty3").value ;
+stock1.innerHTML= product[0].stock - a ;
+stock2.innerHTML= product[1].stock - b ;
+stock3.innerHTML= product[2].stock - c ;
 
 }
 
 function selesai() {
+let name = document.getElementById("index").value;
+let email = document.getElementById("email").value;
+let alamat = document.getElementById("alamat").value;
+let hp = document.getElementById("hp").value;
+let a = document.getElementById("qty1").value;
+let b = document.getElementById("qty2").value;
+let c = document.getElementById("qty3").value;
+
 document.getElementById("sudahselesai").innerHTML = "TERIMA KASIH SUDAH MELAKUKAN ORDER"; 
+document.getElementById("order-nama").innerHTML= "Nama: " + name ;
+document.getElementById("order-email").innerHTML= "Email: " + email ;
+document.getElementById("order-address").innerHTML= "Alamat: " + alamat ;
+document.getElementById("order-phone").innerHTML= "No.Hp: " + hp ;
+
+let total = a * product[0].price + b * product[1].price + c * product[2].price
+document.getElementById("order-detail").innerHTML= "Total order anda Rp " + total ;
 }
 
 console.log(akuorder);
